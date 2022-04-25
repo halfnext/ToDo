6d7ef50599d9dbe03075e11dd1a8b02d6aee76a8
Halfn <Halfnext@gmail.com>
2022-04-25 14:57:30 +0300

    Adding to do

--- controller/Input.controller.js
+++ controller/Input.controller.js
@@ -1,10 +1,31 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function(Controller) {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+], function(Controller, JSONModel, MessageToast) {
     'use strict';
     return Controller.extend("sap.ui.toDo.controller.Input", {
-        post : function () {
-            alert("Hi")
+        onInit : function () {
+            let sPath = sap.ui.require.toUrl("sap/ui/toDo/listItem.json");
+            let oModel = new JSONModel(sPath);
+            this.getView().setModel(oModel);
+        },
+
+        onPost : function (oEvent) {
+            let sValue = oEvent.getParameter("value");
+            let oItem = {
+                task: sValue
+            };
+
+            //send data to model
+            let oModel = this.getView().getModel();
+            let aItems = oModel.getData().listItem;
+            aItems.push(oItem);
+            oModel.setData({
+                listItem: aItems
+            });
+
+            MessageToast.show("Item Added to the List");
         }
     });
 });
--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,21 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function(Controller, MessageToast, JSONModel) {
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
     'use strict';
     return Controller.extend("sap.ui.toDo.controller.Main", {
-        onInit : function () {
-            let oData = {
-                listItem : {
-                    task: "First"
-                }
-            };
-            let oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        }, 
-        onAddItem : function() {
-            MessageToast.show("Item Added to the List");
-        }
     });
 });
