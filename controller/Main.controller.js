2efc84d5ba239f63f8e0e823be022d8f70003cfa
Halfn <Halfnext@gmail.com>
2022-04-25 12:22:24 +0300

    Content managed

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     'use strict';
     return Controller.extend("sap.ui.toDo.controller.Main", {
+        onInit : function () {
+            let oData = {
+                listItem : {
+                    task: "First"
+                }
+            };
+            let oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        }, 
         onAddItem : function() {
-            MessageToast.show("Item Added to List");
+            MessageToast.show("Item Added to the List");
         }
     });
 });
